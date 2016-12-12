73e1c503218d60758a8e451da259288bacc073a9
Vassilis Papanikolaou <vassilisuk@gmail.com>
2016-12-12 02:12:48 +0100

    rename packages.txt to package_list

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get package list from packages.txt file
-export PACKAGE_LIST="packages.txt"
+export PACKAGE_LIST="package_list"
 
 # For inspiration: https://kkovacs.eu/cool-but-obscure-unix-tools
 
