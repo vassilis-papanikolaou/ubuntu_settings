8987705f03aed927b46cc79bb1deff6f605b3104
Vassilis Papanikolaou <vassilisuk@gmail.com>
2016-12-31 13:54:47 +0100

    Configuration scripts: git, ssh

---
+++ configure_git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Setup git
+git config --global user.email "you@example.com"
+git config --global user.name "Vassilis Papanikolaou"
+git config --global core.editor "vim"
--- configure_ssh.sh
+++ configure_ssh.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git config --global user.email "you@example.com"
-git config --global user.name "Vassilis Papanikolaou"
 
 # Generate SSH keys
 ssh-keygen -t rsa -b 4096 -C "vassilis@computer"
