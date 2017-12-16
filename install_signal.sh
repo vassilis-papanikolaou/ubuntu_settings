066f42018944bccb12b4aa75fd6857b2615a3168
Vassilis Papanikolaou <vassilisuk@gmail.com>
2017-12-16 16:01:17 +0100

    Customize 'install_signal.sh' for Ubuntu distros

--- install_signal.sh
+++ install_signal.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Get specific Ubuntu distribution information
+source /etc/lsb-release -c
+
+# Add the 'signal' key
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+# Patch the sources.list file
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt $DISTRIB_CODENAME main" | sudo tee -a /etc/apt/sources.list.d/signal-$DISTRIB_CODENAME.list
+
+# Install signal
 sudo apt update && sudo apt install signal-desktop
